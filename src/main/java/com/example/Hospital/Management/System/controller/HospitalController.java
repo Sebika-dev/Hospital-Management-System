8e6423b3911edcbe80298624b8e8515a1e92eebd
Sebika-dev <sebibirda@gmail.com>
2025-11-01 19:43:57 +0200

    Update HospitalController.java

--- src/main/java/com/example/Hospital/Management/System/controller/HospitalController.java
+++ src/main/java/com/example/Hospital/Management/System/controller/HospitalController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/hospitals")
+@RequestMapping("/hospitals")
 public class HospitalController {
 
     private final HospitalService hospitalService;
