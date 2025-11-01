33313950a7497d8cbce41d12910eff5fe3f15d22
Sebika-dev <sebibirda@gmail.com>
2025-11-01 16:08:34 +0200

    Services

---
+++ src/main/java/com/example/Hospital_Management/System/service/AppointmentService.java
@@ -0,0 +1,35 @@
+package com.example.Hospital_Management.System.service;
+
+import com.example.Hospital_Management.System.model.Appointment;
+import com.example.Hospital_Management.System.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AppointmentService {
+
+    private final AppointmentRepository appointmentRepository;
+
+    @Autowired
+    public AppointmentService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public Appointment saveAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    public List<Appointment> getAllAppointments() {
+        return appointmentRepository.findAll();
+    }
+
+    public Appointment getAppointmentById(String id) {
+        return appointmentRepository.findById(id);
+    }
+
+    public void deleteAppointment(String id) {
+        appointmentRepository.delete(id);
+    }
+}
---
+++ src/main/java/com/example/Hospital_Management/System/service/PatientService.java
@@ -0,0 +1,35 @@
+package com.example.Hospital_Management.System.service;
+
+import com.example.Hospital_Management.System.model.Patient;
+import com.example.Hospital_Management.System.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    @Autowired
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Patient getPatientById(String id) {
+        return patientRepository.findById(id);
+    }
+
+    public void deletePatient(String id) {
+        patientRepository.delete(id);
+    }
+}
